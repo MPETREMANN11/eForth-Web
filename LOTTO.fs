de1effdee21415e6a2e1601fe8f54c9d7869f0b1
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-02-28 11:47:42 +0100

    load LOTTO in /loto/main.fs

---
+++ LOTTO.fs
@@ -0,0 +1,20 @@
+\ define language constants
+0 value FRENCH
+0 value ENGLISH
+
+: inFrench ( -- )
+    -1 to FRENCH
+     0 to ENGLISH
+  ;
+
+: inEnglish ( -- )
+     0 to FRENCH
+    -1 to ENGLISH
+  ;
+
+inEnglish
+
+\ load LOTTO in /loto/main.fs
+s" tools/dumpTool.fs" included? invert 
+[IF] s" tools/dumpTool.fs" included [THEN]
+s" loto/main.fs" included

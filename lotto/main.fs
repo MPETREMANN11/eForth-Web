505cf2c8b9c0e3956a6da846667764c931d82ffa
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-03 21:50:06 +0100

    LOTTO game main file

--- lotto/main.fs
+++ lotto/main.fs
@@ -17,18 +17,24 @@ create --loto
 s" generalWords.fs" included
 
 \ load and link lotto data
-s" euroMillionFR.fs" included
+s" euroMillionFR.fs" included   \ load EURO MILLION (FR) datas
 
 \ update ->nbgrids field
-here getLOTTOdatasAddr -
-nbsPerGrid@ /
-LOTTOdatas ->nbgrids !
+\ here getLOTTOdatasAddr -
+\ nbsPerGrid@ /
+\ LOTTOdatas ->nbgrids !
 
 \ load Manage content of LOTTO grids
 s" gridsManage.fs" included
 
 \ load stats frequency for LOTTO numbers
 s" numbersFrequency.fs" included
+
+\ load text interface for LOTTO program
+s" interface.fs" included
+
+interface   \ run main program
+
 
 
 \ create myNumbers

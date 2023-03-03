76480610e2f40cdc84c934e6f5bf00934671c481
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-03 21:51:51 +0100

    stats frequency for LOTTO numbers

--- lotto/numbersFrequency.fs
+++ lotto/numbersFrequency.fs
@@ -2,7 +2,7 @@
 \ stats frequency for LOTTO numbers
 \    Filename:      numbersFrequency.fs
 \    Date:          28 feb 2023
-\    Updated:       28 feb 2023
+\    Updated:       03 mar 2023
 \    File Version:  1.0
 \    MCU:           eFORTH
 \    Copyright:     Marc PETREMANN
@@ -62,4 +62,52 @@ create frequencyTable
         then
     next
   ;
-.frequency
+
+0 value LOW_NUM
+0 value HIGH_NUM
+
+: searchHighLowFrequency ( -- )
+           1 { lowest_num  }
+ 1 32 lshift { lowest_val  }
+    NR_RANGE { highest_num }
+           0 { highest_val }
+    NR_RANGE for
+        aft
+            \ searches for the most frequently dialed number
+            i getFreqAddr @ dup highest_val >
+            if
+                to highest_val
+                i 1+ to highest_num
+            else
+                drop
+            then
+            \ searches for the number issued the least often
+            i getFreqAddr @ dup lowest_val <
+            if
+                to lowest_val
+                i 1+ to lowest_num
+            else
+                drop
+            then
+        then
+    next
+    highest_num to HIGH_NUM
+     lowest_num to LOW_NUM
+  ;
+
+: .frequHighLow ( -- )
+    searchHighLowFrequency
+    cr
+    FRENCH  ?\ ." numéro sorti le plus souvent....: " 
+    ENGLISH ?\ ." number issued most often........: " 
+    HIGH_NUM ## type ."  ("
+    HIGH_NUM 1- getFreqAddr @ . ."  x)"
+    cr
+    FRENCH  ?\ ." numéro sorti le moins souvent...: " 
+    ENGLISH ?\ ." number issued least often.......: " 
+    LOW_NUM ## type ."  ("
+    LOW_NUM 1- getFreqAddr @ . ."  x)"
+  ;
+
+
+

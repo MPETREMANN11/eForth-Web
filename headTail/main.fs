018bb5cccd2fa22e75df5e6f526f6ccc80aaa935
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-04 10:31:02 +0100

    HEADS and TAILS

---
+++ headTail/main.fs
@@ -0,0 +1,19 @@
+\ *********************************************************************
+\ HEADS and TAILS
+\    Filename:      main.fs
+\    Date:          03 mar 2023
+\    Updated:       03 mar 2023
+\    File Version:  1.0
+\    MCU:           eFORTH
+\    Copyright:     Marc PETREMANN
+\    Author:        Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
+
+\ load general words
+s" HTtrace.fs" included
+
+
+10 multiTrace
+
+

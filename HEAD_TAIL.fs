e7c263fb2f0d966bebf7b6e9884badc281ab1441
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-04 10:33:29 +0100

    load HEAD TAIL in /headTail/main.fs

---
+++ HEAD_TAIL.fs
@@ -0,0 +1,8 @@
+\ pre-load tools
+s" tools/dumpTool.fs" required
+s" tools/condComp.fs" required
+s" tools/random.fs" required
+
+\ load HEAD TAIL in /headTail/main.fs
+s" headTail/main.fs" included
+

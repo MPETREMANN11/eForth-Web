aa28c39c0ceee9688e440257e168c5c413d8122d
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-05 22:09:19 +0100

    load STR ARRAYS in /stringsArray/main.fs

---
+++ STR_ARRAY.fs
@@ -0,0 +1,8 @@
+\ pre-load tools
+\ s" tools/dumpTool.fs" required
+s" tools/condComp.fs" required
+\ s" tools/strings.fs"  required
+
+load STR ARRAYS in /stringsArray/main.fs
+s" stringsArray/main.fs" included
+

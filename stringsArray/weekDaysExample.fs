e0e92227721061a61a0c07c1826186271a64ac59
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-05 22:10:50 +0100

    days of week example

---
+++ stringsArray/weekDaysExample.fs
@@ -0,0 +1,36 @@
+\ *********************************************************************
+\ days of week example
+\    Filename:      weekDaysExample.fs
+\    Date:          05 mar 2023
+\    Updated:       05 mar 2023
+\    File Version:  1.0
+\    MCU:           eFORTH
+\    Copyright:     Marc PETREMANN
+\    Author:        Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
+
+
+7 strArray: days
+  FRENCH [IF]
+    z" Lundi"       zStr!
+    z" Mardi"       zStr!
+    z" Mercredi"    zStr!
+    z" Jeudi"       zStr!
+    z" Vendredi"    zStr!
+    z" Samedi"      zStr!
+    z" Dimanche"    zStr!
+  [ELSE]
+    z" Monday"      zStr!
+    z" Tuesday"     zStr!
+    z" Wednesday"   zStr!
+    z" Thursday "   zStr!
+    z" Friday"      zStr!
+    z" Saturday"    zStr!
+    z" Subday"      zStr!
+  [THEN]
+
+
+0 days zStrType cr
+3 days zStrType cr
+

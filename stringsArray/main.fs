7af4a42c69cf55cea7a4f550493ffb89a71e61d8
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-07 18:03:49 +0100

    ARRAY STRINGS main file

--- stringsArray/main.fs
+++ stringsArray/main.fs
@@ -2,7 +2,7 @@
 \ ARRAY STRINGS main file
 \    Filename:      main.fs
 \    Date:          04 mar 2023
-\    Updated:       04 mar 2023
+\    Updated:       07 mar 2023
 \    File Version:  1.0
 \    MCU:           eFORTH
 \    Copyright:     Marc PETREMANN
@@ -17,5 +17,8 @@ s" generalWords.fs" included
 
 \ load days of week example
 s" weekDaysExample.fs" included
+
+\ load general words
+s" stringsChoice.fs" included
 
 

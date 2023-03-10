c77608a82e22d99e3d90182a1d003f8d00fe70e2
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-10 21:29:57 +0100

    load Graphics Extensions
    load Graphics Extensions in /graphicExtension/main.fs

---
+++ GR_EXT.fs
@@ -0,0 +1,7 @@
+\ pre-load tools
+\ s" tools/dumpTool.fs" required
+\ s" tools/condComp.fs" required
+
+\ load Graphics Extensions in /graphicExtension/main.fs
+s" graphicExtensions/main.fs" included
+

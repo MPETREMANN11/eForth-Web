319f29165d5e4c6ef13198610ec6a38037086dbe
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-10 21:30:36 +0100

    Graphics extensions main file

---
+++ graphicExtension/main.fs
@@ -0,0 +1,20 @@
+\ *********************************************************************
+\ Graphics extensions main file
+\    Filename:      main.fs
+\    Date:          09 mar 2023
+\    Updated:       09 mar 2023
+\    File Version:  1.0
+\    MCU:           eFORTH
+\    Copyright:     Marc PETREMANN
+\    Author:        Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
+
+DEFINED? --grExt [if] forget --grExt  [then]
+create --grExt
+
+\ load graphics extensions
+s" graphicsExtensions.fs" included
+
+\ load graphics tests
+s" graphicsTests.fs" included

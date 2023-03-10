87bd71d1f5b57ecb4bc6bbd5cd9f2c8914a5bf45
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-10 22:48:58 +0100

    Graphics extensions for windows

--- graphicExtension/graphicsExtensions.fs
+++ graphicExtension/graphicsExtensions.fs
@@ -2,7 +2,7 @@
 \ Graphics extensions for windows
 \    Filename:      graphicsExtensions.fs
 \    Date:          09 mar 2023
-\    Updated:       09 mar 2023
+\    Updated:       10 mar 2023
 \    File Version:  1.0
 \    MCU:           eFORTH
 \    Copyright:     Marc PETREMANN
@@ -13,28 +13,40 @@
 
 \ online doc: https://learn.microsoft.com/fr-fr/windows/win32/api/_gdi/
 
-\ in windows vocabulary, use: Gdi32
 
-graphics definitions  windows also
+graphics internals definitions
+windows also
 
-\ La fonction LineTo dessine une ligne de la position actuelle jusqu’à, mais pas y compris, le point spécifié.
-z" LineTo"      3 Gdi32 LineTo      \ hdc x y
+\ La fonction LineTo dessine une ligne de la position actuelle jusqu’à, 
+\ mais pas y compris, le point spécifié.
+z" LineTo"      3 Gdi32 Gdi.LineTo ( hdc x y -- fl )
 
-z" Rectangle"   5 gdi32 Rectangle   \ hdc left top right bottom
+z" Rectangle"   5 gdi32 Gdi.Rectangle   \ hdc left top right bottom
 
-z" Ellipse"     5 gdi32 Ellipse     \ hdc left top right bottom
+z" Ellipse"     5 gdi32 Gdi.Ellipse     \ hdc left top right bottom
 
 \ La fonction CloseFigure ferme une figure ouverte dans un chemin d’accès.
-z" CloseFigure" 1 gdi32 CloseFigure
+z" CloseFigure" 1 gdi32 Gdi.CloseFigure ( hdc --  fl )
 
 \ The GetPixel function retrieves the red, green, blue (RGB) color value 
 \ of the pixel at the specified coordinates.
-z" GetPixel"    3 gdi32 GetPixel    \ hdc x y 
+z" GetPixel"    3 gdi32 Gdi.GetPixel ( hdc x y -- color )
 
+\ The SetPixel function sets the pixel at the specified coordinates 
+\ to the specified color.
+z" SetPixel"    4 gdi32 Gdi.SetPixel ( hdc x y colorref -- colorref )
 
+only forth 
+graphics definitions internals
 
-
-only forth definitions
-
-
+: lineTo ( x y -- )
+    hdc -rot Gdi.LineTo drop ;
+: rectangle ( left top right bottom -- )
+    >r >r >r >r hdc r> r> r> r> Gdi.Rectangle drop ;
+: closeFigure ( -- )
+    hdc Gdi.CloseFigure drop ;
+: getPixel ( x y -- colorref )
+    hdc -rot Gdi.GetPixel ;
+: setPixel ( x y color -- )
+    hdc -rot Gdi.GetPixel ;
 

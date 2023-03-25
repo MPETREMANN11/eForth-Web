89bb96d44f8692f0b71723f1d8434bba2541d928
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-03-25 21:32:50 +0100

    additional definitions for eFORTH web

--- web/additionalDefs.fs
+++ web/additionalDefs.fs
@@ -2,7 +2,7 @@
 \ additional definitions for eFORTH web
 \    Filename:      additionalDefs.fs
 \    Date:          14 mar 2023
-\    Updated:       18 mar 2023
+\    Updated:       25 mar 2023
 \    File Version:  1.0
 \    Forth:         eFORTH web
 \    Author:        Marc PETREMANN
@@ -30,7 +30,23 @@ JSWORD: time@ { -- h m s }
 
 \ *** GRAPHIC extensions words *************************************************
 
-\ draw ellipse
+JSWORD: arc { x y r a0 ax div }
+  context.ctx.arc(x, y, r, (Math.PI * 2 * a0) / div, (Math.PI * 2 * ax) / div);
+~
+\ usage:
+\ beginPath  100 100 60   0  90 360 arc stroke
+
+: circle { x y r }
+    x y r 0 360 360 arc
+  ;
+
+
+\ clear rectangle
+JSWORD: clearRect { x y width height }
+    context.ctx.clearRect(x, y, width, height);
+~
+
+
 \ draw ellipse
 JSWORD: ellipse { x y rx ry angle div }
   context.ctx.ellipse(x, y, rx, ry, Math.PI * 2 * angle / div, 0, 2 * Math.PI);
@@ -40,22 +56,37 @@ JSWORD: ellipse { x y rx ry angle div }
 \ 100 100 75 30 0 360 ellipse stroke
 
 
+JSWORD: globalAlpha! { val div }
+    context.ctx.globalAlpha = val/div;
+~
+\ usage, draw semi transparent rect:
+\ 7 10 globalAlpha!
+\ $ffffff color!
+\ 10 10 100 20 rect fill
+
+
 \ write text in graphic mode
-JSWORD: fillText { a n x y -- }
-  context.ctx.fillText(GetString(a, n), x, y);
+JSWORD: strokeText { a n x y -- }
+  context.ctx.strokeText(GetString(a, n), x, y);
 ~
 \ usage
 \ $ff0000 color!
-\ s" test" 20 30 fillText
+\ s" test" 20 30 strokeText
 
 
+\ draw rectangle
+JSWORD: rect { x y width height }
+    context.ctx.rect(x, y, width, height);
+~
+\ usage:
+\ $0000ff color!
+\ 10 10 100 20 rect stroke
 
-\ @TODO: Autres fonctions à creuser:
-\ fillStyle
-\ rect
-\ strokeRect
-\     ctx.strokeStyle = "green";
-\     ctx.strokeRect(20, 10, 160, 100);
+
+\ reset translate and rotate
+JSWORD: resetTransform { }
+  context.ctx.resetTransform();
+~
 
 
 

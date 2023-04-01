8e2f9b82c45c1119f3e1b13571b33e99c078abd2
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-04-01 22:36:01 +0200

    configuration for anim01.fs

---
+++ web/ANIM01/fs/config.fs
@@ -0,0 +1,29 @@
+\ *********************************************************************
+\ configuration for anim01.fs
+\    Filename:      config.fs
+\    Date:          01 apr 2023
+\    Updated:       01 apr 2023
+\    File Version:  1.0
+\    Forth:         eFORTH all versions 7.x++
+\    Copyright:     Eric Fð
+\    Author:        Eric Fð
+\    Adaptation:    Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
+
+\ canvas size
+600 constant ctxWidth 
+400 constant ctxHeight 
+
+\ colors definitions
+$000000 constant COLOR_BLACK
+$0000ff constant COLOR_BLUE
+$ff0000 constant COLOR_RED
+
+\ button color default value
+COLOR_BLUE value BUTTON_COLOR
+
+\ button size default values
+80 value BUTTON_WIDTH
+20 value BUTTON_HEIGHT
+

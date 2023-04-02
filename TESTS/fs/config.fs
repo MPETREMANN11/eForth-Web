f41c2564ec57fd7ff5855d80ca9036bcfb3fc904
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-04-02 18:13:39 +0200

    Update config.fs

--- TESTS/fs/config.fs
+++ TESTS/fs/config.fs
@@ -3,5 +3,9 @@
 800 value ctxWidth
 300 value ctxHeight
 
+$000000 constant COLOR_BLACK
+$FFFFFF constant COLOR_WHITE
+$808080 constant COLOR_Gray
 $D3D3D3 constant COLOR_LightGray
 
+$ff0000 constant COLOR_RED

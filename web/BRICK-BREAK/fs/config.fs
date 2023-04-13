7c3defcb3cfb0293f77598cabec7b387893cf9f7
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-04-13 21:31:32 +0200

    Create config.fs

---
+++ web/BRICK-BREAK/fs/config.fs
@@ -0,0 +1,18 @@
+
+\ define canvas size
+600 value ctxWidth
+300 value ctxHeight
+
+$000000 constant COLOR_BLACK
+$FFFFFF constant COLOR_WHITE
+$808080 constant COLOR_Gray
+$D3D3D3 constant COLOR_LightGray
+
+$ff0000 constant COLOR_RED
+$0000ff constant COLOR_BLUE
+
+\ BALL parameters
+$0095DD constant BALL_COLOR
+     10 constant BALL_SIZE
+
+

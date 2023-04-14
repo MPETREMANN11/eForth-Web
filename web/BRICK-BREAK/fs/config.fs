bb5f18bb1d9b1e645dc5dd17d74c6bf1f30ae357
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-04-14 12:58:16 +0200

    Update config.fs

--- web/BRICK-BREAK/fs/config.fs
+++ web/BRICK-BREAK/fs/config.fs
@@ -3,16 +3,19 @@
 600 value ctxWidth
 300 value ctxHeight
 
-$000000 constant COLOR_BLACK
-$FFFFFF constant COLOR_WHITE
-$808080 constant COLOR_Gray
-$D3D3D3 constant COLOR_LightGray
-
-$ff0000 constant COLOR_RED
-$0000ff constant COLOR_BLUE
+\ $000000 constant COLOR_BLACK
+\ $FFFFFF constant COLOR_WHITE
+\ $808080 constant COLOR_Gray
+\ $D3D3D3 constant COLOR_LightGray
+\ 
+\ $ff0000 constant COLOR_RED
+\ $0000ff constant COLOR_BLUE
 
 \ BALL parameters
 $0095DD constant BALL_COLOR
      10 constant BALL_SIZE
 
-
+\ RACQUET parameters
+$0075BD constant RACQUET_COLOR
+     50 constant RACQUET_WIDTH
+     10 constant RACQUET_HEIGHT

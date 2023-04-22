2f8f82495e47eebb22dc14741e1299397904ec54
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-04-22 15:49:39 +0200

    Create config.fs

---
+++ web/MATH-CURVES/fs/config.fs
@@ -0,0 +1,31 @@
+
+\ define canvas size
+600 value ctxWidth
+300 value ctxHeight
+
+\ define step ofd grid
+20 value GRID_STEP
+
+\ define equation scale
+GRID_STEP value EQUATION_SCALE
+
+
+$000000 constant COLOR_BLACK
+$FFFFFF constant COLOR_WHITE
+$808080 constant COLOR_Gray
+$D3D3D3 constant COLOR_LightGray
+$DCDCDC constant COLOR_Gainsboro
+
+$ff0000 constant COLOR_RED
+$FFC0CB constant COLOR_PINK
+$FFC0CB constant COLOR_ORANGE
+$FFFF00 constant COLOR_YELLOW
+$0000FF constant COLOR_BLUE
+$800080 constant COLOR_PURPLE
+$008000 constant COLOR_GREEN
+$A52A2A constant COLOR_BROWN
+
+ctxWidth  2/ value CANVAS_X_MIDDLE
+ctxheight 2/ value CANVAS_Y_MIDDLE
+
+

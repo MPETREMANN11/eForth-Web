c76e611b0a6c3b885d43ca38106d34523334ba42
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-04-24 21:16:41 +0200

    Update linearEquation.fs

--- web/MATH-CURVES/fs/linearEquation.fs
+++ web/MATH-CURVES/fs/linearEquation.fs
@@ -2,7 +2,7 @@
 \ definition of linear equation
 \    Filename:      linearEquation.fs
 \    Date:          21 apr 2023
-\    Updated:       21 apr 2023
+\    Updated:       23 apr 2023
 \    File Version:  1.0
 \    Forth:         eFORTH web
 \    Author:        Marc PETREMANN
@@ -13,19 +13,19 @@ fvariable a
 fvariable b
 
 \ linear equation
-: ax+b { x -- y }
-    x s>f                   \ convert x in float value
+: ax+b ( x -- y )
+    s>f                     \ convert x in float value
     a SF@  F*   b SF@  F+   \ calc: ax+b
     f>s
   ;
-    
+     
 : line.equat1 ( -- )
-    0.5e Fscale a SF!
-      2e Fscale b SF!
+    0.5e Fscale* a SF!
+      2e Fscale* b SF!
   ;
-
+ 
 : line.equat2 ( -- )
-     -1e Fscale a SF!
-      5e Fscale b SF!
+     -1e Fscale* a SF!
+      5e Fscale* b SF!
   ;
 

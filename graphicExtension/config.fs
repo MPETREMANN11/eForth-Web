b81dfec4f5177d8c9ab89aa98636c79173234c18
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-05-24 09:01:04 +0200

    Configuration for Graphics extensions tests

---
+++ graphicExtension/config.fs
@@ -0,0 +1,33 @@
+\ *********************************************************************
+\ Configuration for Graphics extensions tests
+\    Filename:      config.fs
+\    Date:          24 may 2023
+\    Updated:       24 may 2023
+\    File Version:  1.0
+\    MCU:           eFORTH windows
+\    Copyright:     Marc PETREMANN
+\    Author:        Marc PETREMANN
+\    GNU General Public License
+\ *********************************************************************
+
+
+\ define colors constants
+$000000 constant COLOR_BLACK
+$FFFFFF constant COLOR_WHITE
+$808080 constant COLOR_GRAY
+$D3D3D3 constant COLOR_LightGray
+
+$ff0000 constant COLOR_RED
+$FFC0CB constant COLOR_PINK
+$FFC0CB constant COLOR_ORANGE
+$FFFF00 constant COLOR_YELLOW
+$0000FF constant COLOR_BLUE
+$800080 constant COLOR_PURPLE
+$008000 constant COLOR_GREEN
+$A52A2A constant COLOR_BROWN
+
+\ define window dimension
+600 constant WINDOW_WIDTH
+300 constant WINDOW_HEIGHT
+
+
